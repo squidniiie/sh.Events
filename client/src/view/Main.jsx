3aa11270d2d02615fee5b2bb2be419557ec11857
Sydnie <sydniegreenwood@gmail.com>
2021-11-19 14:01:13 -0800

    commit

--- client/src/view/Main.jsx
+++ client/src/view/Main.jsx
@@ -4,10 +4,10 @@ import '../static/Main.css'
 
 function Main() {
     return (
-        <div>
+        <div className="mainContainer">
             <div className="main">
                 <div className="row">
-                    <img className="hero col" src="https://cdn.pixabay.com/photo/2021/10/19/21/31/landscape-6724639_1280.jpg" alt="hero img" />
+                    <img className="hero col" src="https://images.unsplash.com/photo-1607749111659-e1c8e05f5f24?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1000&q=80" alt="hero img" />
                     <div className="hero col">
                         <div className="hero card">
                             <h5 style={{ fontStyle: "italic" }}>Building a Diversely Likeminded Community</h5>
