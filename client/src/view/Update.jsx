91fd0f4dea368e4042842cc6fe7edddca0dfa61a
Amanda Cable <amandahaggard94@gmail.com>
2021-11-19 14:23:02 -0800

    commit

--- client/src/view/Update.jsx
+++ client/src/view/Update.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 import { useParams, useHistory } from 'react-router-dom'
-import '../static/Create.css'
+import '../static/Update.css'
 
 const Update = () => {
 
